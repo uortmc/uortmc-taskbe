abdd73004228c43952034cf0a25c603cc81752ff
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-02-11 11:08:50 +0000

    [134] Scan Model

--- taskbackend/taskbackendapp/models.py
+++ taskbackend/taskbackendapp/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import *
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+import uuid
 
-# Create your models here.
+class Scan(Model):
+    ALGORITHMS = [
+        ('NS', 'NOT SET'),
+        ('SVC', 'Simple Support Vector Machine')
+    ]
+    token = UUIDField('Scan Token',unique=True, default=uuid.uuid4, editable=False)
+    algorithm=CharField('Algorithm Used',max_length=10,choices=ALGORITHMS,default=ALGORITHMS[0][0])
+
+
+    def __str__(self):
+        return "Scan "+str(self.token)
