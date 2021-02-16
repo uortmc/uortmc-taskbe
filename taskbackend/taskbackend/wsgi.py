7e78748d0d07e791c1c0f418ab827c60cd5bf502
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-02-16 01:45:09 +0000

    [146] dotenv-file tested

--- taskbackend/taskbackend/wsgi.py
+++ taskbackend/taskbackend/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskbackend.settings')
 
+project_folder = os.path.expanduser('.')                #Get full path
+load_dotenv(os.path.join(project_folder, '.env'))       #append .env and load dotenv
 application = get_wsgi_application()
