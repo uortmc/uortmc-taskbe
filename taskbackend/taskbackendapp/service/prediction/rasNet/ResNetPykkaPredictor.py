b78bc8710527f2574060a219442e940da9b11044
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-04-19 20:13:15 +0100

    189-typos-RAS-RES-fixed

--- taskbackend/taskbackendapp/service/prediction/rasNet/ResNetPykkaPredictor.py
+++ taskbackend/taskbackendapp/service/prediction/rasNet/ResNetPykkaPredictor.py
@@ -33,7 +33,7 @@ class ResNetPykkaPredictor(pykka.ThreadingActor, AbstractPredictor):
         self.model.load_state_dict(torch.load(f'resnet18.tch', map_location=torch.device('cpu')))
         self.model.eval()
         self.model = self.model.to(self.device)
-        self.logger = logging.getLogger("RasNetPykkaPredictor")
+        self.logger = logging.getLogger("ResNetPykkaPredictor")
         self.logger.setLevel(logging.NOTSET)
         self.logbuffer = ""
         super().__init__()
@@ -55,7 +55,7 @@ class ResNetPykkaPredictor(pykka.ThreadingActor, AbstractPredictor):
         try:
             img=self.image_transformer(self.decodeImage(scan.image))
             img = transforms.functional.crop(img, 8, 87, 301, 385)
-            self.loggingWithScan(scan,"RASNET Algorithm initialized")
+            self.loggingWithScan(scan,"RESNET Algorithm initialized")
             prediction = self.model(img).argmax().item()
             self.loggingWithScan(scan, "Model predicted: "+str(prediction))
             if prediction == 0:
